fcd74f6ecebba6697578d05d8fcdbeafb51dee65
Nasir Nishat <nasirnishat00@gmail.com>
2026-03-30 12:52:00 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
+import { resolve } from 'path';
+
 export default {
   root: './',
   base: '/project/',
   server: {
     port: 4000,
     cors: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        crossyRoad: resolve(__dirname, 'crossy-road/index.html'),
+        tunnelBoat: resolve(__dirname, 'tunnel-boat/index.html'),
+        globe: resolve(__dirname, '3d-globe/index.html'),
+        neonTunnel: resolve(__dirname, 'neon-tunnel/index.html')
+      }
+    }
   }
 }
